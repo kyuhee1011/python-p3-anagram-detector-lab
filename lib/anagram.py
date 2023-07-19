c8e4c99799f5cfee4edda767606add15f6d0682c
Kyuhee Lee <kyuhee.lee.1011@gmail.com>
2023-07-19 03:10:46 -0400

    finish

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word): 
+        self.word=word
+    def match (self, list):
+        newList=[]
+        new_match_word=[letter for letter in self.word]
+        for word in list:
+            if sorted ([letter for letter in word])==sorted (new_match_word):
+                newList.append(word)
+        return newList
